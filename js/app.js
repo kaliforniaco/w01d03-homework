1afc161dad2650f4ec9506b4bbff65da0e58c9dd
David Epps <kaliforniaco@gmail.com>
2018-06-13 20:12:21 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -16,4 +16,23 @@ for (let i=0; i<201; i+=2){
 
 }
 
-//-
+//-logged evens 0-200
+
+//3. Excited Kitten
+
+const kitten=["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+for (let i=0; i<20; i++){
+	
+	mew = Math.floor(Math.random() * 3);
+
+	if (i % 2 === 0){
+		console.log("Love me, pet me HSSSSSSS!" + kitten[mew]);
+	}
+	else{
+		console.log("Love me, pet me HSSSSSSS!")
+	}
+}
+
+//-logged 20 HSSSSSSS, even RND*3 " "
+
